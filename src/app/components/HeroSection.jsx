58dcf884ceca6e629f5414b0f0ad112e73ed15d1
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2024-08-15 21:37:27 +0530

    hero section+type animation

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,52 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+
+import { TypeAnimation } from 'react-type-animation';
+
+
+
+
+const HeroSection = () => {
+  return (
+    <section>
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+            <div className='col-span-7 place-self-center text-center sm:text-left'>
+             <h1 className="text-white mb-4 mt-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold"> <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 '>Hello, I'm {""}</span>
+             <br></br>
+             <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Thasneem',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'Web Developer',
+        1000,
+        'UI/UX Designer',
+        1000,
+      ]}
+      wrapper="span"
+      speed={50}
+      repeat={Infinity}
+    /></h1>
+             <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptates porro eveniet? Neque architecto repellat magnam dolores sapiente? Autem, magni.l</p>
+        <div>
+            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white' >Hire Me</button>
+            <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3'><span className="block  bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV </span></button>
+        </div>
+        </div>
+        <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+            <div className='rounded-full bg-[#181818] relative w-[250px] h-[250px]px  lg:w-[250px] h-[250px] '>
+            <Image
+            src="/images/pfp.png"
+            className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+            alt="hero image"
+        width={240}
+        height={250}/>
+           </div>
+ </div>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
