841dd0ed6f9617351354625ae9187ee2436efae4
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2024-08-17 20:12:52 +0530

    projects animation added

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import Image from 'next/image'
-
+import { motion } from "framer-motion";
 import { TypeAnimation } from 'react-type-animation';
 
 
@@ -11,7 +11,12 @@ const HeroSection = () => {
   return (
     <section>
         <div className="grid grid-cols-1 lg:grid-cols-12">
-            <div className='col-span-7 place-self-center text-center sm:text-left'>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
              <h1 className="text-white mb-4 mt-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold"> <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 '>Hello, I'm {""}</span>
              <br></br>
              <TypeAnimation
@@ -33,9 +38,13 @@ const HeroSection = () => {
             <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white' >Hire Me</button>
             <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3'><span className="block  bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV </span></button>
         </div>
-        </div>
-        <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
-            <div className='rounded-full bg-[#181818] relative w-[250px] h-[250px]px  lg:w-[250px] h-[250px] '>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >            <div className='rounded-full bg-[#181818] relative w-[250px] h-[250px]px  lg:w-[250px] h-[250px] '>
             <Image
             src="/images/pfp.png"
             className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
@@ -43,7 +52,7 @@ const HeroSection = () => {
         width={240}
         height={250}/>
            </div>
- </div>
+ </motion.div>
         </div>
     </section>
   )
--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,7 +1,8 @@
 "use client"
-import React,{useState} from 'react'
+import React,{useState, useRef} from 'react'
 import { ProjectCard } from './ProjectCard'
 import ProjectTag from './ProjectTag';
+import { motion, useInView } from "framer-motion";
 const projectsData = [
     {
       id: 1,
@@ -60,12 +61,18 @@ const projectsData = [
 
 export const ProjectSection = () => {
     const [tag,setTag]=useState("All");
+    const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
     const handleTagChange=(newTag)=>{
         setTag(newTag)
     }
     const filteredProjects=projectsData.filter((project)=>{
         return project.tag.includes(tag)
     })
+    const cardVariants = {
+        initial: { y: 50, opacity: 0 },
+        animate: { y: 0, opacity: 1 },
+      };
   
   return (
     <>
@@ -76,8 +83,26 @@ export const ProjectSection = () => {
 
     </div>
   
-    <div className='grid md:grid-cols-3 gap-8 md:gap-12'>{filteredProjects.map((project)=>
-        <ProjectCard key={project.id} title={project.title} description={project.description} imgUrl={project.image} gitUrl={project.gitUrl} previewUrl={project.previewUrl}/>)}</div>
+    <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
     </>
   )
 }
