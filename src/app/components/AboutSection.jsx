af93ddbbd25f71517ebb725800fb6b46d4512045
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2024-08-19 07:20:45 +0530

    image update

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -51,7 +51,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
         <div className='md:grid md:grid-cols-2 gap-8 items-senter py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-        <Image src="/portfolio-nextjs/images/about.jpg" width={500} height={500} alt="about image"/>
+        <Image src="./images/about.jpg" width={500} height={500} alt="about image"/>
         <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
             <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
             <p className='text-base lg:text-lg'>I'm Thasneem, a programmer with a passion for web development. My journey into web development has been driven by a commitment to deliver the best designs, I strive to bring ideas to life and exceed expectations. When I'm not coding, you can find me painting and reading fiction. Let's collaborate and turn your visions into reality!</p>
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -46,7 +46,7 @@ const HeroSection = () => {
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >            <div className='rounded-full bg-[#181818] relative w-[250px] h-[250px]px  lg:w-[250px] h-[250px] '>
             <Image
-            src="/portfolio-nextjs/images/pfp.png"
+            src="./images/pfp.png"
             className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
             alt="hero image"
         width={240}
