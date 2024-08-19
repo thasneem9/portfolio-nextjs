5ba6ccf7aec17b4235d6b224cb46856df3a667a9
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2024-08-19 06:30:02 +0530

    installed next-deploy and updated next.confg

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      unoptimized: true, // GitHub Pages doesn't support dynamic image optimization
+    },
+    basePath: '/portfolio-nextjs', 
+    assetPrefix: '/portfolio-nextjs/', 
+    trailingSlash: true,
+  };
+  
+  export default nextConfig;
+  
