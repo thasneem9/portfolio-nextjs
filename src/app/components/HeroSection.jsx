d7bc69ee510abb6e64bfab2a478bf0c17724c741
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2024-08-19 08:17:01 +0530

    image path error

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -37,7 +37,7 @@ const HeroSection = () => {
         <div>
             <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white' >Hire Me</button>
             <a
-  href="./images/image.pdf"
+  href="/images/image.pdf"
   download="image.pdf"
 >
   <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
